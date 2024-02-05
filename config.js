08130cca2bd52c13d9c6a1d52e173186a5c3ef0a
zaidnizam <138511600+zaidnizam@users.noreply.github.com>
2024-02-05 17:53:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -50,17 +50,17 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'Hacxk'
+global.botname = 'HACXK BOT'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.menuvid = ''
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
-global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
-global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
+global.packname = 'HACXK┃ᴮᴼᵀ' 
+global.author = '@zaid' 
+global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
+global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/sss_ghost\n' 
+global.dygp = 'https://chat.whatsapp.com/'
+global.fgsc = 'https://github.com/' 
+global.fgyt = 'https://youtube.com/'
+global.fgpyp = 'https://youtube.com/'
+global.fglog = 'https://raw.githubusercontent.com/' 
 global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
 
